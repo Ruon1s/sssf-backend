71bbd53096531d2711e9d66489b7a55ef3af1c7b
Mikael Ruonala <Mikael.Ruonala@Metropolia.fi>
2021-04-24 11:14:00 +0300

    Schemas and models

--- models/Entries.js
+++ models/Entries.js
@@ -1,2 +1,16 @@
 'use strict';
 
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const entriesSchema = new Schema ({
+    Entryname: String,
+    // image: Image,
+    Ingredients: String,
+    Steps: String,
+    Rating: Number,
+    Date: Date, //might need to swap
+    UserID: String
+});
+
+export default mongoose.model('Entries', entriesSchema);
--- schemas/entriesSchema.js
+++ schemas/entriesSchema.js
@@ -1,2 +1,57 @@
 'use strict';
 
+import {gql} from 'apollo-server-express';
+
+
+export default gql`
+    extend type Query {
+    entries(): [Entry],
+    entriesByUser(id: ID!): [Station]
+    }
+    
+    type Entry {
+        id: ID
+        Entryname: String
+        Image: Image
+        Ingredients: String
+        Steps: String
+        Rating: Int
+        Date: Date
+        userID: ID
+    }
+    
+    extend type Mutation{
+    deleteEntry(id: ID!): Entry
+    addEntry(
+        Entryname: String
+        Image: Image
+        Ingredients: String
+        Steps: String
+        Rating: Int
+        Date: Date
+        userID: ID
+        ): Entry
+    modifyEntry(
+        Entryname: String
+        Image: Image
+        Ingredients: String
+        Steps: String
+        Rating: Int
+    ): Entry    
+    }
+
+
+    extend type Mutation {
+    deleteStation(id: ID!): Station
+    addStation(
+    Connections: [Connections]
+    Postcode: String
+    Title: String
+    AddressLine1: String
+    StateOrProvince: String
+    Town: String
+    Location: newLocation
+    ): Station
+    }
+    
+`
