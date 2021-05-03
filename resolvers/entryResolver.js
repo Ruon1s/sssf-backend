c72812e7475b8bb4bbd935fc519636b8f6771f65
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2021-05-03 10:15:55 +0300

    tweaks

--- resolvers/entryResolver.js
+++ resolvers/entryResolver.js
@@ -74,10 +74,10 @@ export default {
                     console.log('pathname', pathName);
                     await stream.pipe(fs.createWriteStream(pathName));
                     const photourl = {
-                        url: `/home/jelastic/ROOT/public/${filename}`
+                        url: `https://charge.jelastic.metropolia.fi/${filename}`
                     };
                     let modifyEntry = {...args, File: photourl.url};
-                 /*   const oldEntry = await Entries.findById(args.id);
+                    const oldEntry = await Entries.findById(args.id);
                     if (oldEntry.File !== photourl.url) {
                         console.log('entry to delete', oldEntry);
                         const oldfilename = oldEntry.File.replace(/^.*(\\|\/|\:)/, '');
@@ -85,7 +85,7 @@ export default {
                         await fs.unlink(`/home/jelastic/ROOT/public_html/${oldfilename}`, (err) => {
                             console.log(err)
                         });
-                    } */
+                    }
                     return await Entries.findByIdAndUpdate(args.id, modifyEntry, {new: true})
 
                 }
